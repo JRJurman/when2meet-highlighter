99365c7c086046944553874b8a4dd16aa83e0628
Jesse Jurman <jesse.jurman@hudl.com>
2015-07-22 13:53:46 -0500

    added highlighter

---
+++ highlighter.js
@@ -0,0 +1,11 @@
+// just insert the function in your browser of choice, and run
+// example: highlighter("jesse", "blue")
+highlighter = function(username, color) {
+  AvailableAtSlot.forEach( function(e, i) {
+    if (e.indexOf(PeopleIDs[PeopleNames.indexOf(username)]) != -1) { 
+      var divDOM = document.getElementById("GroupTime"+TimeOfSlot[i]);
+      divDOM.style.background = color
+    console.log(divDOM);
+    };
+  });
+}
