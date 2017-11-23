2abff6eee61852dcb82d44a7131dae8e097be0f7
jjurman <jjurman@constantcontact.com>
2017-11-23 14:57:32 -0500

    warnings, case insensitive, return dates

--- highlighter.js
+++ highlighter.js
@@ -1,10 +1,29 @@
 // just insert the function in your browser of choice, and run
 // example: highlighter("jesse", "blue")
-highlighter = function(username, color) {
-  AvailableAtSlot.forEach( function(e, i) {
-    if (e.indexOf(PeopleIDs[PeopleNames.indexOf(username)]) != -1) { 
-      var divDOM = document.getElementById("GroupTime"+TimeOfSlot[i]);
-      divDOM.style.background = color;
-    };
-  });
+const highlighter = function(username, color) {
+
+  // verify Name is in PeopleNames
+  const lowerPeopleNames = PeopleNames.map(name => name.toLowerCase())
+  const lowerUsername = username.toLowerCase()
+  if (lowerPeopleNames.indexOf(lowerUsername) === -1) {
+    throw new Error(`Name ${lowerUsername} not found`)
+  }
+
+  // map slots to the index, if it is the name we're looking for
+  // then, filter out null slots
+  var slotsForUserName = AvailableAtSlot
+    .map( (slotIDs, slotIndex) => slotIDs.indexOf(PeopleIDs[lowerPeopleNames.indexOf(lowerUsername)]) !== -1 ? slotIndex : null )
+    .filter( slotIndex => slotIndex )
+
+  // color each of the slots with the color
+  slotsForUserName.forEach( (slotIndex) => {
+    const divDOM = document.getElementById("GroupTime"+TimeOfSlot[slotIndex])
+    divDOM.style.background = color
+  })
+
+  // return all the times for this person
+  return slotsForUserName.map( slotIndex => {
+    return new Date(TimeOfSlot[slotIndex] * 1000)
+  })
+
 }
