c4dc684d859e35cd87c024ecdb498f42ae4ac710
jjurman <jjurman@constantcontact.com>
2017-11-28 02:01:35 -0500

    Make a chrome extension

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
-# when2meet-people-highlighter
-Helpful script that highlights a single persons availability on when2meet
+# when2meet-highlighter
+Chrome Extension that allows users to visually see the indiviudal availability of people on the when2meet web app (https://www.when2meet.com).
+
+This is extremely useful if you have a single person that has to be at a meeting (regardless of how many other people can show).
 
 ## Instructions
-Copy the script in highlighter.js and run it in your browsers console. Then you can use the highlighter function with a username and standard css background color.
+Install the Chrome Extension. The names of all the participants should appear at the bottom. Selecting any name should tint all the times that person is available.
 
 ## Example
 ![image](https://user-images.githubusercontent.com/326557/33244537-73133eee-d2c7-11e7-87cb-3573ccfd59cf.png)
---
+++ menu.js
@@ -0,0 +1,83 @@
+// highlight function
+// takes in a name, and the number of degrees to rotate the hue
+const highlight = (name, deg) => {
+  // get the id for username
+  const userId = PeopleIDs[PeopleNames.indexOf(name)]
+
+  // map slots to the index, if it contains the id we're looking for
+  // then, filter out null slots
+  AvailableAtSlot
+    .map( (slotIDs, slotIndex) => slotIDs.indexOf(userId) !== -1 ? slotIndex : null )
+    .filter( slotIndex => slotIndex !== null )
+    .forEach( slotIndex => {
+      // grab the DOM element for this slotId and apply the css style
+      const divDOM = document.getElementById(`GroupTime${TimeOfSlot[slotIndex]}`)
+      divDOM.style.filter = `hue-rotate(${deg}deg)`
+
+      // if the color is white, we need to change it so that hue-rotate works
+      if (divDOM.style.background === 'rgb(255, 255, 255)') {
+        divDOM.style.background = '#ddeed5'
+        divDOM.fakeColor = true
+      }
+    })
+}
+
+// resetColors function
+// resets all the colors back to normal
+// Note: can't really do a per-user basis, because highlighting users overlaps
+// it would require significant logic, and state management
+const resetColors = () => {
+  AvailableAtSlot
+    .forEach( (slot, slotIndex) => {
+      const divDOM = document.getElementById(`GroupTime${TimeOfSlot[slotIndex]}`)
+      divDOM.style.filter = ''
+      // if the color was faked, we need to change it back to white
+      if (divDOM.fakeColor) {
+        divDOM.style.background = 'rgb(255, 255, 255)'
+        divDOM.fakeColor = false
+      }
+    })
+}
+
+const buttonStyle = (on, hue) => `
+  font-size: 1em;
+  border-radius: 0.5em;
+  border-color: #339900;
+  border-width: 0.15em;
+  color: ${on ? 'white' : '#339900'};
+  background-color: ${on ? '#339900' : 'white'};
+  cursor: pointer;
+  outline: none;
+  margin: 0.25em;
+  filter: hue-rotate(${hue}deg);
+`
+
+const main = document.getElementById('MainBody')
+const menu = document.createElement('div')
+menu.style.padding = '1em'
+
+PeopleNames.map((name, index) => {
+  const hue = (index+1)*100
+  const button = document.createElement('button')
+  button.innerText = name
+  button.style.cssText = buttonStyle(false, hue)
+  button.highlight = highlight.bind(this, name, hue)
+  button.reset = resetColors.bind(this)
+  button.isHighlighted = false
+  button.onclick = () => {
+    if (button.isHighlighted) {
+      // reset cells
+      resetColors()
+      button.style.cssText = buttonStyle(false, hue)
+      button.isHighlighted = false
+    } else {
+      // highlight cells
+      highlight(name, hue)
+      button.style.cssText = buttonStyle(true, hue)
+      button.isHighlighted = true
+    }
+  }
+  return button
+}).forEach(button => menu.appendChild(button))
+
+main.appendChild(menu)
